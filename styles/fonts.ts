fccdc2a055ba43a1f8a3d3a32a951c884dfec955
Dayropo <gboyega.banjo@gmail.com>
2025-04-05 09:56:40 +0100

    initial commit

---
+++ styles/fonts.ts
@@ -0,0 +1,12 @@
+import { Bubblegum_Sans, Montserrat } from "next/font/google"
+
+export const bubblegumSans = Bubblegum_Sans({
+  subsets: ["latin"],
+  variable: "--font-bubblegum-sans",
+  weight: ["400"],
+})
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+})
